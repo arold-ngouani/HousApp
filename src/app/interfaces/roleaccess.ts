122b79da8a4b3934c2b2fae0e24601c9f2f656f6
Arold Ngouani Yapteu <aroldngouani@MacBook-Air-de-Arold.local>
2023-11-12 22:05:01 +0100

    interfaces

---
+++ src/app/interfaces/roleaccess.ts
@@ -0,0 +1,7 @@
+export interface Roleaccess {
+    role: string,
+    menu: string,
+    haveedit: boolean,
+    haveadd: boolean,
+    havedelete: boolean
+}
---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: string,
+    name: string,
+    password: string,
+    email: string,
+    gender: string,
+    role: string,
+    isactive: boolean
+}
